c5075a4fc1fa06b966c933f4600ff4897053d642
Jon Toshmatov from HP <jontoshmatov@yahoo.com>
2020-10-13 10:28:29 -0400

    saving before creating italim branch

--- projects/good-it-fellas/october12.php
+++ projects/good-it-fellas/october12.php
@@ -1,17 +1,8 @@
 <?php
-echo "<pre>";
-require_once "../database.php";
-$db = new \Database\database("uzdirectory");
+$cars = ['Alfa', 'Audi','BMW'];
+$mycars = implode('<hr>', $cars);
 
-$news_items = $db->sql("SELECT title FROM world_news limit 20;");
-
- foreach ($news_items as $item){
-
-     $title = $item['title'];
-     $new_title = str_replace("Virginia", 'Vir***',$title);
-
-     echo "<hr>{$new_title}<hr>";
- }
+echo $mycars;
 ?>
 
 
