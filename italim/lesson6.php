26b36334af3c2365205ec9be20d946135b0b10e2
Jon Toshmatov from HP <jontoshmatov@yahoo.com>
2020-11-24 07:14:36 -0500

    saving lesson6

---
+++ italim/lesson6.php
@@ -0,0 +1,21 @@
+<?php
+$provinces = ["Toshkent", "Andijon", "Namangan","Farg'ona", "Sirdaryo", "Jizzax","Samarqand", "Buxoro","Navoiy","Qashqadaryo","Surxondaryo","Xorazm"];
+$ferghana = ["Oltiariq tumani","Bag'dod tumani","Beshariq tumani","Buvayda tumani","Dang'ara tumani","Farg'ona tumani","Furqat tumani","Qo'shtepa tumani","Quva tumani","Rishton tumani","So'x tumani","Toshloq tumani","Uchko'prik tumani","O'zbekiston tumani","Yozyovon tumani"];
+//Sort arrays || Сортировать массивы || Massivlarni saralash
+//sort($provinces);
+//rsort($provinces);
+//asort($provinces);
+//ksort($provinces);
+//krsort($provinces);
+echo "<pre>";
+//$result = array_search("Farg'ona",$provinces); //answer is 3
+//If we want to print Farg'ona on the screen
+//If we want to print random array on the screen
+//Split an array into chunks
+//Extract a slice of the array
+//Merge arrays
+//$boys = ['Jon', 'Alisher','Max','Hamidjon','Thomas','Jeff'];
+//$girls = ['Lola','Soliha','Evelyn','Camilla','Sabo','Shakhzoda','Shakhnoza'];
+$numbers = [1,2,3,4,5];
+$result = array_sum($numbers);
+print_r($result);
