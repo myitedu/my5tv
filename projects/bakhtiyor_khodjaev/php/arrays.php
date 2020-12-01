e046f31f9900f91c7ae945b3fd22372e157109e2
Jon Toshmatov from HP <jontoshmatov@yahoo.com>
2020-12-01 15:56:46 -0500

    saving chart

--- projects/bakhtiyor_khodjaev/php/arrays.php
+++ projects/bakhtiyor_khodjaev/php/arrays.php
@@ -1,26 +1,89 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="/bootstrap/css/bootstrap.css">
+</head>
+<body>
 <?php
-$file = file_get_contents("../../cars.txt");
-$cars = explode(",", $file);
-foreach ($cars as $car){
-    echo "<div class='car'><a href='https://www.google.com/search?q=$car'>$car</a></div>";
-}
+$nums = [10,90,100,200,185];
+$left = 100;
+$height = 200;
 ?>
+<div class="title2">Our Chart</div>
+<div id="chart">
+    <div class="title">MYITEDU CHART</div>
+    <?php
+    foreach ($nums as $num){
+        $height = $num;
+    ?>
+    <div style="left: <?php echo $left;?>px; height:<?php echo $height;?>px" class="bar"><?php echo $num;?></div>
+    <?php
+        $left+=50;
+    }
+    ?>
+
+    <table class="table">
+        <tr>
+            <td>&nbsp</td>
+            <td>&nbsp</td>
+            <td>&nbsp</td>
+        </tr>
+        <tr>
+            <td>&nbsp</td>
+            <td>&nbsp</td>
+            <td>&nbsp</td>
+        </tr>
+        <tr>
+            <td>&nbsp</td>
+            <td>&nbsp</td>
+            <td>&nbsp</td>
+        </tr>
+        <tr>
+            <td>&nbsp</td>
+            <td>&nbsp</td>
+            <td>&nbsp</td>
+        </tr>
+    </table>
+</div>
+
 <style>
-    .car{
-        width: auto;
-        height: 40px;
-        margin: 5px;
-        display: inline-block;
+    .bar{
+        width: 30px;
+        border: 1px solid grey;
+        background-color: #5890ff;
+        position: absolute;
+        display: block;
+        z-index: 100;
+        margin-right: 20px;
+        bottom: 287px;
+    }
+    .title2{
+        position: relative;
+        left: 51px;
+        top: 205px;
+        writing-mode: vertical-rl;
+    }
+    .title{
         text-align: center;
-        font-size: 25px;
-        color: black;
-        border: 1px solid black;
-        background-color: yellow;
+        margin-bottom: 20px;
     }
-    .car:hover, a:hover{
-        background-color: #0a2998;
-        color: white !important;
-        cursor: pointer;
+    #chart{
+        width: 700px;
+        margin: 30px auto;
     }
+td:first-child{
+    border-left: 2px solid black;
+}
+tr:last-child td{
+    border-bottom: 2px solid black;
+}
+
 </style>
 
+</body>
+</html>
