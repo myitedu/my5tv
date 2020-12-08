d3d68bc9dd82673838532aa116e32c42dab2dac1
Jon Toshmatov from HP <jontoshmatov@yahoo.com>
2020-12-07 22:26:38 -0500

    saving

--- quizzes/backend.php
+++ quizzes/backend.php
@@ -21,7 +21,12 @@ RIGHT JOIN answers AS a
 ON q.id = a.question_id
 WHERE q.quiz_id = 1;";
 $items = $db->sql($sql);
+$answered_questions = $db->sql("SELECT * FROM users_answers AS ua WHERE ua.question_id=$q AND user_id = 1;");
 $questions = [];
+$answered_id = null;
+if (!empty($answered_questions[0]['answer_id'])){
+    $answered_id = $answered_questions[0]['answer_id'];
+}
 foreach ($items as $item) {
     $questions[$item['qid']][] = $item;
 }
@@ -36,17 +41,38 @@ foreach ($questions as $question_id => $answers) {
                 <div class="question_title"><?php echo $answers[0]['question']; ?></div>
                 <div class="question_counter">
                     Question <span class="current_page_number"><?php echo $q . "</span> of " . count($questions); ?>
+                        <br>
+                        <div class="time_lapsed"></div>
                 </div>
                 <div class="error">An error has occured</div>
-                <?php foreach ($answers as $answer) { ?>
-                    <div class="answers">
+                <?php
+                foreach ($answers as $answer) {
+                    $answered_class = null;
+                    if ($answer['aid'] == $answered_id){
+                        $answered_class = "answered_class";
+                    }else{
+                        $answered_class = null;
+                    }
+                    ?>
+                    <div class="answers <?php echo $answered_class;?>">
                         <table class="table answers_table">
                             <tr>
                                 <td>&nbsp;</td>
-                                <td><label title="Your answer"><input value="<?php echo $answer['aid'] ?>"
-                                                                      name="answer_<?php echo $question_id; ?>"
-                                                                      type="radio"> <?php echo $answer['answers']; ?>
-                                    </label></td>
+                                <td>
+                                    <label title="Your answer">
+                                        <?php
+                                        if ($answer['aid'] == $answered_id){
+                                        ?>
+                                        <input checked value="<?php echo $answer['aid'] ?>" name="answer_<?php echo $question_id; ?>" type="radio"> <?php echo $answer['answers']; ?>
+                                            <?php
+                                        }else{
+                                            ?>
+                                        <input value="<?php echo $answer['aid'] ?>" name="answer_<?php echo $question_id; ?>" type="radio"> <?php echo $answer['answers']; ?>
+                                        <?php
+                                        }
+                                            ?>
+                                    </label>
+                                </td>
                             </tr>
                         </table>
                     </div>
--- quizzes/js/scripts.js
+++ quizzes/js/scripts.js
@@ -4,7 +4,6 @@ $(function (){
     let next_page_number = 1;
     let prev_page_number = 1;
     $("#quiz").load("backend.php");
-
     $("#left_button").click(function (){
         let qid = $(".question").data("id");
         let total = $(".question").data("total");
@@ -45,4 +44,34 @@ $(function (){
            $("#left_button").show();
        }
     });
+
+    let seconds = 0;
+    let minute = 0;
+    let hour = 0;
+    let time_limit = 0;
+    let timer = setInterval(time_lapsed,1000);
+    function time_lapsed(){
+        //var d = new Date();
+        //d.toLocaleTimeString();
+        seconds++;
+        time_limit++;
+        if (seconds%60===0){
+            minute++;
+            seconds=0;
+        }
+
+        if (minute%60===0 && minute>1){
+            hour++;
+            minute = 0;
+        }
+        let time_out_class = null;
+        if (time_limit>=600){
+            time_out_class = "time_out_class";
+        }
+        console.log(time_limit);
+        let hour_glass_image = "<img title='Your session timed out' class='hour_glass_image "+time_out_class+"' src='https://webapp.icbf.com/v2/app/img/hpAlertGlass.gif'>";
+        $(".time_lapsed").html(hour + " hour "+minute+" mins and "+seconds+" seconds "+hour_glass_image);
+
+    }
+
 });
