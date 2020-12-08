c0fbd52872d89b85867831a21a583577685b7942
Jon Toshmatov from HP <jontoshmatov@yahoo.com>
2020-12-07 21:23:29 -0500

    saving

--- quizzes/backend.php
+++ quizzes/backend.php
@@ -1,56 +1,59 @@
 <?php
 require_once "database.php";
 $parms = $_POST;
-$q = $_GET['q']??1;
-$q = (int) $q;
+$q = $_GET['q'] ?? 1;
+$q = (int)$q;
 $user_id = 1;
 $db = new \Database\database("myitedu");
 if (!empty($parms['formdata'])) {
- $answer = $parms['formdata'];
- $p = explode("=", $answer);
- $question_id = explode("_", $p[0]);
- $question_id = $question_id[1]??0;
- $question_id = (int) $question_id;
- $answer_id = $p[1]??0;
- $answer_id = (int) $answer_id;
- $sql = "INSERT INTO users_answers (user_id, question_id, answer_id) VALUES ($user_id, $question_id,$answer_id);";
- $saved = $db->sql($sql);
+    $answer = $parms['formdata'];
+    $p = explode("=", $answer);
+    $question_id = explode("_", $p[0]);
+    $question_id = $question_id[1] ?? 0;
+    $question_id = (int)$question_id;
+    $answer_id = $p[1] ?? 0;
+    $answer_id = (int)$answer_id;
+    $sql = "INSERT INTO users_answers (user_id, question_id, answer_id) VALUES ($user_id, $question_id,$answer_id);";
+    $saved = $db->sql($sql);
 }
-
-
 $sql = "SELECT q.id AS qid, q.question, q.quiz_id, a.question_id AS question_id, a.id AS aid, a.answers FROM questions AS q
 RIGHT JOIN answers AS a
 ON q.id = a.question_id
 WHERE q.quiz_id = 1;";
 $items = $db->sql($sql);
 $questions = [];
-foreach ($items as $item){
+foreach ($items as $item) {
     $questions[$item['qid']][] = $item;
 }
 $num = 0;
-foreach ($questions as $question_id=>$answers){
+foreach ($questions as $question_id => $answers) {
     $num++;
-if ($num === $q){
-?>
-<form>
-<div data-total="<?php echo count($questions);?>" data-id="<?php echo $num;?>" id="<?php echo 'question_'.$num; ?>" class="question">
-    <div class="question_title"><?php echo $answers[0]['question'];?></div>
-    <div class="question_counter">
-         Question <span class="current_page_number"><?php echo $q."</span> of ".count($questions);?>
-    </div>
-    <div class="error">An error has occured</div>
-    <?php foreach ($answers as $answer){?>
-    <div class="answers">
-        <table class="table answers_table">
-            <tr>
-                <td>&nbsp;</td>
-                <td><label title="Your answer"><input value="<?php echo $answer['aid']?>" name="answer_<?php echo $question_id;?>" type="radio"> <?php echo $answer['answers'];?> </label></td></tr>
-        </table>
-</div>
-    <?php }?>
-</div>
-</form>
-    <?php
-}
+    if ($num === $q) {
+        ?>
+        <form>
+            <div data-total="<?php echo count($questions); ?>" data-id="<?php echo $num; ?>"
+                 id="<?php echo 'question_' . $num; ?>" class="question">
+                <div class="question_title"><?php echo $answers[0]['question']; ?></div>
+                <div class="question_counter">
+                    Question <span class="current_page_number"><?php echo $q . "</span> of " . count($questions); ?>
+                </div>
+                <div class="error">An error has occured</div>
+                <?php foreach ($answers as $answer) { ?>
+                    <div class="answers">
+                        <table class="table answers_table">
+                            <tr>
+                                <td>&nbsp;</td>
+                                <td><label title="Your answer"><input value="<?php echo $answer['aid'] ?>"
+                                                                      name="answer_<?php echo $question_id; ?>"
+                                                                      type="radio"> <?php echo $answer['answers']; ?>
+                                    </label></td>
+                            </tr>
+                        </table>
+                    </div>
+                <?php } ?>
+            </div>
+        </form>
+        <?php
+    }
 }
 ?>
--- quizzes/js/scripts.js
+++ quizzes/js/scripts.js
@@ -3,8 +3,6 @@ $(function (){
     let total = $(".question").data("total");
     let next_page_number = 1;
     let prev_page_number = 1;
-
-
     $("#quiz").load("backend.php");
 
     $("#left_button").click(function (){
@@ -15,19 +13,36 @@ $(function (){
         }
         qid--;
         $("#quiz").load("backend.php?q="+qid);
+       if (qid<=total){
+           $("#right_button").show();
+       }
+       if (qid == 1){
+           $(this).hide();
+       }
     });
-
-    //The next button
     $("#right_button").click(function (){
         let formdata = $('form').serialize();
         let qid = $(".question").data("id");
         let total = $(".question").data("total");
+        if (qid==total){
+
+            let ask = confirm("Are you ready to submit your answers?");
+            if (ask){
+                let myqid = qid++;
+                $("#quiz").load("success.php?q="+myqid+"&total="+total);
+                $(this).hide();
+                return false;
+            }else{
+                return false;
+            }
+        }
         if (qid==total+1){
             return false;
         }
         qid++;
-
         $("#quiz").load("backend.php?q="+qid, {formdata});
+       if (qid>1){
+           $("#left_button").show();
+       }
     });
-
 });
