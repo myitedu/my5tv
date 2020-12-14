6f36d82f5621396d77d9947b4950ceb10b12c6bd
MYITEDU <myitedu@localhosts>
2020-12-14 11:58:38 -0500

    saving

---
+++ projects/medical/temperature/index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="/js/jquery-3.5.1.js"></script>
+</head>
+<body>
+<?php
+$degree = rand(10,100);
+?>
+<div class="container-fluid">
+
+<div id="temperature">
+    <div id="degree"></div>
+</div>
+
+</div>
+
+</body>
+</html>
