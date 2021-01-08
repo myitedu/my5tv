e9bcda1611a3af26d1127a7b74fb7394721654d9
MYITEDU <myitedu@localhosts>
2021-01-08 12:12:45 -0500

    Intro to HTML

---
+++ fall2020/frontend/jontoshmatov.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Jon Toshmatov's Website</title>
+</head>
+<body>
+
+<h1>Hammaga Salom</h1>
+<hr>
+<img id="image1" class="myimage" src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/nature-quotes-1557340276.jpg?crop=1.00xw:0.757xh;0,0.0958xh&resize=1200:*">
+<br>
+<img id="image2" class="myimage" src="https://e2k9ube.cloudimg.io/s/cdn/x/https://edienet.s3.amazonaws.com/news/images/full_40419.jpg?v=15/06/2020%2016:52:00">
+<hr>
+<img class="myimage" src="https://www.carscoops.com/wp-content/uploads/2019/08/e4df1d4a-2002-mercedes-benz-g-class-swb-kahn-tuning-0.jpg">
+
+<p></p>
+
+
+
+<style>
+
+    /*
+    1) ID
+    2) Class
+    3) HTML element name: img, hr, p, div, span
+     */
+
+
+    #image1{
+
+    }
+
+    #image2{
+
+    }
+
+    .myimage{
+        width: 300px;
+        height: 300px;
+        border: 5px dotted lightgreen;
+        margin: 10px;
+        border-radius: 50%;
+    }
+
+
+
+
+body{
+    background-color: #406686;
+    color: #0ac1ec;
+}
+</style>
+
+
+</body>
+</html>
