0147279135e4838d34696da902fbd4900994d0cb
Jon Toshmatov from HP <jontoshmatov@yahoo.com>
2021-01-15 11:10:58 -0500

    January 15 for Fall2020

---
+++ fall2020/frontend/jontoshmatov/january15.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>January 15, 2021</title>
+</head>
+<body>
+<div id="mybox">
+    My Box
+</div>
+
+<style>
+    #mybox{
+        width: 500px;
+        height: 500px;
+        background-color: lightgrey;
+        border: 1px solid black;
+        margin: 100px auto;
+        border-radius: 50%;
+        text-align: center;
+        line-height: 460px;
+        font-size: 40px;
+        color: darkred;
+        background-image: url("https://images1.miaminewtimes.com/imager/u/original/11600690/miami_beach_south_beach_ocean_sand.jpg");
+        background-size: 100% 100%;
+        border: 10px double purple;
+    }
+</style>
+
+</body>
+</html>
