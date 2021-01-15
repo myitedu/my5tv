17250573d7f304d45d0ae5ea7408c2c6c87b8683
Jon Toshmatov from HP <jontoshmatov@yahoo.com>
2021-01-15 11:54:00 -0500

    January 15 for Fall2020 css positions

--- fall2020/frontend/jontoshmatov/january15.php
+++ fall2020/frontend/jontoshmatov/january15.php
@@ -5,32 +5,42 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>CSS positions</title>
 </head>
 <body>
-<div id="living_room">
-    <iframe id="my_youtube" src="https://www.youtube.com/embed/-uvp7_qVGDY" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
+
+<div id="parent">
+    <div id="child1">Child 1</div>
+    <div id="child2">Child 2</div>
+    <div id="child3">Child 3</div>
 </div>
 
 <style>
-    #living_room{
-        width: 870px;
-        height: 520px;
+    #parent{
+        background-color: #5890ff;
+        width: 500px;
         margin: auto;
-        background-image: url(https://cdn2.expertreviews.co.uk/sites/expertreviews/files/2019/12/jvc_fire_tv_edition.jpg);
-        background-size: 100% 100%;
-        background-repeat: no-repeat;
-        border: 10px double darkblue;
+    }
+    #child1{
+        background-color: green;
+        width: 33%;
+        display: inline-block;
+        float: left;
+        text-align: center;
+    }
+    #child2{
+        background-color: purple;
+        width: 34%;
+        display: inline-block;
+        float: left;
+        text-align: center;
     }
 
-    #my_youtube{
-        width: 335px;
-        height: 207px;
-        position: relative;
-        top: 111px;
-        left: 260px;
-        border: none;
-
+    #child3{
+        background-color: #ec9e6d;
+        width: 33%;
+        display: inline-block;
+        text-align: center;
     }
 </style>
 
