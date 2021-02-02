5c145c072f5ced81fac5c82984f534acb584083a
Jon Toshmatov from HP <jontoshmatov@yahoo.com>
2021-02-02 11:00:05 -0500

    saving

---
+++ fall2020/frontend/jontoshmatov/february2.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>February 2, 2021</title>
+    <script src="/js/jquery-3.5.1.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../css/february2.css">
+</head>
+<body>
+<div class="container-fluid">
+<div id="wrapper">
+    <header>
+        <?php require_once "navbar.php"; ?>
+    </header>
+    <?php require_once "breadcumb.php"; ?>
+    <div id="mycontent">
+        <?php require_once "carousel.php"; ?>
+        <?php require_once "accordion.php"; ?>
+        <?php require_once "cards.php"; ?>
+    </div>
+    <footer>
+        <img class="social_media_icons" src="https://gnb-user-uploads.s3.amazonaws.com/template_values/deatha/website_images/content/9-why-choose-dah/social-media-icons.png">
+    </footer>
+</div>
+</div>
+</body>
+</html>
