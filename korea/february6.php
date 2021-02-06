eb6e07d490f7147add151bbae8462fe9863185b7
Jon Toshmatov from HP <jontoshmatov@yahoo.com>
2021-02-06 09:55:26 -0500

    saving korea team lesson

--- korea/february6.php
+++ korea/february6.php
@@ -1,29 +1,49 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
 <?php
-$persons = [
+$numbers = range(1, 10000);
 
-    [
-        'name' => 'Jon',
-        'age' => 21,
-        'countries' => ['South Korea','Uzbekistan'=>["Tashkent",'Ferghana',"Namangan","Andijan"],'Russia','USA'],
-    ],
-    [
-        'name' => 'Jessica',
-        'age' => 23,
-        'countries' => ['South Korea','Uzbekistan'=>["Tashkent",'Ferghana',"Namangan","Andijan"],'Russia','USA',"Japan"],
-        'cars' => ["BMW","MERCEDES","AUDI","Nexia","CHEVY TAHOE"]
-    ],
-    [
-        'name' => 'Zarina',
-        'age' => 34,
-        'countries' => ['South Korea','Uzbekistan'=>["Tashkent",'Ferghana',"Namangan","Andijan"],'Russia','USA'],
-    ],
-    [
-        'name' => 'Temur',
-        'age' => 65,
-        'countries' => ['South Korea','Uzbekistan'=>["Tashkent",'Ferghana',"Namangan","Andijan"],'Russia','USA'],
-    ],
+foreach ($numbers as $number){
+    if ($number%2===0){
+        echo "<div class='even_numbers'>$number</div>";
+    }else{
+        echo "<div class='odd_numbers'>$number</div>";
+    }
 
-];
-foreach($persons as $person){
-echo $person['name'].", ";
 }
+?>
+
+<style>
+    .even_numbers{
+        color: red;
+        background-color: yellow;
+        padding: 5px;
+        margin: 5px;
+        border: 1px solid black;
+        width: auto;
+        text-align: center;
+        display: inline-block;
+    }
+    .odd_numbers{
+        color: #f3e188;
+        background-color: #1717ee;
+        padding: 5px;
+        margin: 5px;
+        border: 1px solid black;
+        width: auto;
+        text-align: center;
+        display: inline-block;
+    }
+</style>
+
+
+</body>
+</html>
