d7b434403d77a3481f764e382db5e8e0796b2447
Jon Toshmatov from HP <jontoshmatov@yahoo.com>
2021-02-10 18:34:28 -0500

    saving MYITEDU2021 class notes 2

--- myitedu2021/jontoshmatov/february10.php
+++ myitedu2021/jontoshmatov/february10.php
@@ -1,9 +1,25 @@
 <?php
-define('STUDENTS',[
-    'name' => 'Jessica',
-    'company' => 'Google',
-    'hobby' => 'cooking food'
-]);
+$arrays =
+[
+    'USA',
+    'Russia',
+    'Uzbekistan'=>[
+        'Tashkent' => ['Mirzo Ulugbek','Olmazor','Yunusobod','Chilonzor'],
+        'Ferghana'=> ['Margilan','Kokand','Navbahor'],
+        'Samarkand'
+    ],
+    'Brazil'
+];
+echo "Before the code dies <hr>";
+exit('I have a mesage.. our enemies have 4 batalion and 3 tanks');
+echo "After the code died";
 
 
-echo "My name is ".STUDENTS['name']." and I love ".STUDENTS['hobby']." in my free time";
+
+echo "<pre>";
+print_r(sizeof($arrays['Uzbekistan']['Tashkent']));
+echo "</pre>";
+
+
+
+
