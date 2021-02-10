54a551a58ff39448eea3d12a551d3dc18a5eda1a
Jon Toshmatov from HP <jontoshmatov@yahoo.com>
2021-02-10 10:25:10 -0500

    saving Korea jamoasini code 4

--- myitedu2021/jontoshmatov/february10.php
+++ myitedu2021/jontoshmatov/february10.php
@@ -1,6 +1,7 @@
 <?php
 
 //For Loop
+/*
 for($i=1; $i<=100; $i++){
     if (find_even_numbers($i)){
         echo "Even:  $i <hr>";
@@ -12,5 +13,37 @@ for($i=1; $i<=100; $i++){
 function find_even_numbers($number){
     return ($number % 2 === 0); //Boolean TRUE or FALSE;
 }
+*/
 
 //While  Loop
+/*
+$i = 1;
+while ($i < 100){
+    if (find_even_numbers($i)){
+        echo "Even:  $i <hr>";
+    }else{
+        echo "$i <hr>";
+    }
+    $i++;
+}
+function find_even_numbers($number){
+    return ($number % 2 === 0); //Boolean TRUE or FALSE;
+}
+*/
+//Do While Loop
+/*
+$i = 200;
+
+do {
+    if (find_even_numbers($i)){
+        echo "Even:  $i <hr>";
+    }else{
+        echo "$i <hr>";
+    }
+   $i++;
+}while ($i<100);
+
+function find_even_numbers($number){
+    return ($number % 2 === 0); //Boolean TRUE or FALSE;
+}
+*/
