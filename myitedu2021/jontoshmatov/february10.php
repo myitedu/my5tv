ce069a1694f7672dda509257db408b386333416b
Jon Toshmatov from HP <jontoshmatov@yahoo.com>
2021-02-10 10:17:51 -0500

    saving Korea jamoasini code 3

--- myitedu2021/jontoshmatov/february10.php
+++ myitedu2021/jontoshmatov/february10.php
@@ -1,53 +1,16 @@
 <?php
-#echo "Min: " . min(2, 87,23,5, 7);
-#echo "<hr>";
-#echo "Max: " . max(354, 87,1000,5, 7);
-//$num = -3883.7;
-//echo abs($num);
-//$num = 25.4;
-//echo round($num);
-//echo rand(1,5);
-//$num = 6.3;
-//echo floor($num);
-/*if (){
 
-}elseif(){
-
-}elseif(){
-
-}elseif(){
-
-}elseif(){
-
-}elseif(){
-
-}elseif(){
-
-}else{
-
-}*/
-
-
-$car = 'Nexia';
-
-switch ($car){
-    case 'BMW':
-        echo 'This is a German car';
-        break;
-
-    case 'Honda':
-        echo 'This is a Japanese car';
-        break;
-
-    case 'Nexia':
-        echo uzbek_cars();
-        break;
-
-    default:
-        echo 'This is other car';
-        break;
+//For Loop
+for($i=1; $i<=100; $i++){
+    if (find_even_numbers($i)){
+        echo "Even:  $i <hr>";
+    }else{
+        echo "$i <hr>";
+    }
 }
 
-function uzbek_cars(){
-    return "All cars are made in either Andjian or Samarkand";
+function find_even_numbers($number){
+    return ($number % 2 === 0); //Boolean TRUE or FALSE;
 }
+
+//While  Loop
