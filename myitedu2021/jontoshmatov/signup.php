d9166a7cb1321bb0f1873c3467fa349689b6fcb3
MYITEDU <myitedu@localhosts>
2021-02-24 19:05:17 -0500

    saving

--- myitedu2021/jontoshmatov/signup.php
+++ myitedu2021/jontoshmatov/signup.php
@@ -1,39 +1,42 @@
 <?php
+/*
 $inputs = $_POST ?? null;
 $validated = false;
 $email = $inputs['email'] ?? null;
 $age = $inputs['age'] ?? null;
+*/
 
-var_dump($age);
-
-echo "<hr>";
-
-echo $age;
-
-exit;
+$cars = ['Audi','BMW','GMC','Honda','Mercedes','Toyota'];
 
 
-//$age = (int) $age;
+//tiger, lion, elephant, zebra, volves
+//eat, hunt, play, sleep
 
+foreach ($cars as $car){
+    echo drive_car($car)."<hr>";
 
-$is_email_valid = filter_var($email, FILTER_VALIDATE_EMAIL);
-if (!$is_email_valid) {
-    $msg = "Sorry, your email is in a wrong format";
-    header("Location: february24.php?msg=$msg");
-    exit($msg);
 }
-echo "GOOD EMAIL<hr>";
-var_dump($age);
-exit;
-//$visitors = ['jontoshmatov11@yahoo.com','jon@jon.com','test@test.com','jessica@gmail.com','tosh@gmail.com'];
-//$found = in_array($email, $visitors);
-if (!$validated) {
-    $msg = "Sorry, your email is not registered";
-    header("Location: february24.php?msg=$msg");
-    exit($msg);
+
+
+function drive_car($car){
+    echo park_car($car);
+    return "$car is driving.";
 }
-echo "Welcome and you are registered with us";
 
-?>
 
-<script>alert('Hello Site Owner! \n Your site has been hacked by @Yang Yii! \n Until you make a ransome, your visitors will be redirected to our site!');document.location = "http://myitedu.us?ref=educational+purposes+only";</script>
+function stop_car($car){
+    return "$car is breaking | ";
+}
+
+function park_car($car){
+    echo backup_car($car);
+    return "$car is parking | ";
+}
+
+function backup_car($car){
+    echo stop_car($car);
+    return "$car is backing up | ";
+}
+
+
+
