c2dbaebc168e052e40d73996dacfdcd635310486
Jon Toshmatov from HP <jontoshmatov@yahoo.com>
2021-03-03 10:25:13 -0500

    savcing calculator frontend for Korea team

---
+++ korea/jontoshmatov/calculator/index.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="css/calculator.css">
+</head>
+<body>
+<div class="container-fluid">
+    <div id="calculator">
+        <table class="table table-bordered">
+            <tr>
+                <td class="cal_display" colspan="4">432</td>
+            </tr>
+            <tr>
+                <td class="calc_num">C</td>
+                <td class="calc_num">+/-</td>
+                <td class="calc_num">%</td>
+                <td class="calc_sign">/</td>
+            </tr>
+            <tr>
+                <td class="calc_num">7</td>
+                <td class="calc_num">8</td>
+                <td class="calc_num">9</td>
+                <td class="calc_sign">x</td>
+            </tr>
+            <tr>
+                <td class="calc_num">4</td>
+                <td class="calc_num">5</td>
+                <td class="calc_num">6</td>
+                <td class="calc_sign">-</td>
+            </tr>
+            <tr>
+                <td class="calc_num">1</td>
+                <td class="calc_num">2</td>
+                <td class="calc_num">3</td>
+                <td class="calc_sign">+</td>
+            </tr>
+            <tr>
+                <td class="calc_num" colspan="2">0</td>
+                <td class="calc_num">.</td>
+                <td class="calc_sign">=</td>
+            </tr>
+        </table>
+    </div>
+</div>
+</body>
+</html>
