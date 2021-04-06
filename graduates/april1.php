afcfa22f2b8ffa5119c605da95cd20494b97c4a3
Jon Toshmatov from HP <jontoshmatov@yahoo.com>
2021-04-06 19:42:01 -0400

    merging

---
+++ graduates/april1.php
@@ -0,0 +1,13 @@
+<?php
+//header('HTTP/1.1 200 OK');
+//header('HTTP/1.1 404 Not Found');
+//header('HTTP/1.1 403 Forbidden');
+header('HTTP/1.1 404 HEY DUMMY WATCH WHAT YOU ARE TYPING');
+//header('HTTP/1.1 304 Not Modified');
+//header('HTTP/1.1 500 Internal Server Error');
+
+//header('Location: http://www.example.org/');
+//header('Refresh: 10; url=http://www.example.org/');
+print 'You will be redirected in 10 seconds';
+
+echo $name;
---
+++ graduates/cybersecurity.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <meta name="author" content="Jon Toshmatov">
+    <meta name="description" content="This page is about Jon Toshmatov who become a very interested in CS from childhood">
+    <meta name="keyword" content="cs, jon, toshmatov, uzbek, america, clarke, harvard, myitedu">
+</head>
+<body>
+<img alt="PHP is a web development language" src="/img/slide100000.jpg">
+</body>
+</html>
