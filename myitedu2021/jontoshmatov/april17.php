e9284c0e904ec4623163dbf32af0b1b76dda1646
Jon Toshmatov from HP <jontoshmatov@yahoo.com>
2021-04-17 12:26:30 -0400

    calendar is working now

--- myitedu2021/jontoshmatov/april17.php
+++ myitedu2021/jontoshmatov/april17.php
@@ -9,14 +9,31 @@
     <link rel="stylesheet" href="/bootstrap/css/bootstrap.css">
 </head>
 <body>
+
+<?php
+$month=$_GET['month']??date('m');
+$year=$_GET['year']??date('Y');
+$year = (int) $year;
+$month = (int) $month;
+if ($year<1900 || $year>2200){
+    $year = date('Y');
+}
+if ($month<1 || $month>12){
+    $month = date('m');
+}
+$today_date = date('d');
+$last_day = date('t');
+$month_name = date('F', strtotime("$month/$today_date/$year"));
+$first_weekday_number = date('w', strtotime("$month/1/$year"));
+?>
 <div id="calendar">
     <table class="table table-bordered">
         <tr>
-            <th colspan="7">April 2021</th>
+            <th colspan="7"><?php echo $month_name." ".$year;?> </th>
         </tr>
         <?php
         $rows = range(1, 6);
-        $cols = range(1, 7);
+        $cols = range(0, 6);
         $counter = 0;
         $counter2 = 1;
         foreach ($rows as $row) {
@@ -34,15 +51,16 @@
             echo "<tr>";
 
             foreach ($cols as $col) {
-                $counter++;
-                if ($counter>4){
+
+                if ($counter>$first_weekday_number){
                     $counter2++;
                 }
-                if ($counter<4 || $counter2>30){
+                if ($counter<$first_weekday_number || $counter2>$last_day){
                     echo "<td>&nbsp;</td >";
                 }else{
                     echo "<td>$counter2</td >";
                 }
+                $counter++;
 
             }
             echo "</tr >";
