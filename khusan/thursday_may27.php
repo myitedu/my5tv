a41849bca326c7fcfcec6e51b93b00a189903c96
Admin <jontoshmatov@yahoo.com>
2021-05-27 19:32:30 -0400

    saving Khusan's code for May 27 2021

---
+++ khusan/thursday_may27.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Howdy Partner!</title>
+</head>
+<body>
+
+<a target="_blank" href="http://google.com">Google.com</a> |
+<a target="_blank" href="http://facebook.com">Facebook.com</a> |
+<a target="_blank" href="http://instagram.com">Instagram.com</a>
+<hr>
+
+<ol>
+    <li>Get up in the morning</li>
+    <li>Have breakfast</li>
+    <li>Read the newspaper</li>
+    <li>Call parents</li>
+    <li>Go to work</li>
+    <li>Pick up some groceries and laundry</li>
+    <li>Eat supper</li>
+    <li>Go to gym</li>
+    <li>Shower</li>
+    <li>Go to bed</li>
+</ol>
+
+
+<h1>Welcome to MYITEDU.US!!!</h1>
+<h2>Welcome to MYITEDU.US!!!</h2>
+<h3>Welcome to MYITEDU.US!!!</h3>
+<h4>Welcome to MYITEDU.US!!!</h4>
+<h5>Welcome to MYITEDU.US!!!</h5>
+<h6>Welcome to MYITEDU.US!!!</h6>
+
+<hr>
+
+<p>My name is Jon Toshmatov</p>
+<p>
+    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+    when an unknown printer took a galley of type and scrambled it to make a type specimen book
+</p>
+<p>
+    It has survived not only five centuries, but also the leap into electronic typesetting,
+    remaining essentially unchanged.  It was popularised in the 1960s with the release of Letraset sheets
+    containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker
+    including versions of Lorem Ipsum.
+</p>
+<hr>
+
+<img title="This was supposed to my car" alt="This is Range Rover" src="https://m.atcdn.co.uk/a/media/w540/0c5f6a5a658a496f87398c88b82cfd24_doesnotexist.jpg">
+<hr>
+<img title="Mercedes Benz S 550 Class" src="/img/Mercedes-Benz-S-Class-1200.jpg">
+
+</body>
+</html>
